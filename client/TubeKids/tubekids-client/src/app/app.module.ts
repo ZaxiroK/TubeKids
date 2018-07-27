af133bd94e02a092fb515ef0c5b9d30b2d71a96e
Edwin <ejimenezu@est.utn.ac.cr>
2018-07-26 23:54:33 -0600

    Se crea el componente user
    Se crea el companente user y se agrega bootstrap

---
+++ client/TubeKids/tubekids-client/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserComponent
+  ],
+  imports: [
+    BrowserModule,
+    NgbModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
